8e59de285438787a5f064066b8791ed96e443243
PaulaAraya <paula.arayawilliams@gmail.com>
2018-06-27 12:15:29 -0300

    avances en el selector de tarjetas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
-let bip = null;
+const select = document.querySelector("BipCardSelection")
+const inputText = document.querySelector("input")
+let bipCards = null;
 
-fetch("http://www.psep.cl/api/Bip.php?&numberBip=666")
+
+fetch("http://www.psep.cl/api/Bip.php?&numberBip=666")//Solicitud de la data de las tarjetas
 .then(response => response.json())
-.then(bipJASON => {
-  bip = bipJASON
-  console.log(bip);
+.then(bipJSON => {
+  bipCards = bipJSON
+  console.log(bipCards);
+  renderInfoBip(bipCards);
 })
-.catch()
+.catch(error => {
+  console.error("tarjeta no existe");
+  console.error("ERROR > " + error.stack);
+});
 
+renderInfoBip = (bipCards) => {
+  if (bipCards) {
+    let innerHtml = "";
+
+    for (let i = 0; i < bipCards.length; i++) {
+      let bip = bipCards[i];
+
+      if (bip) {
+        innerHtml += "<option value='" + i + "'>" + bipCards["N&ordm; tarjeta bip! "] + "</option>";
+      }
+    }
+    bipSelect.innerHtml = innerHtml
+
+inputText.addEventListener() => {
+
+}
+
+select.addEventListener(change, () )=>{
+
+}
