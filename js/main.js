cdc25bca2a88b2fe22b9dbccc8ad5fc06ee0c4b7
PaulaAraya <paula.arayawilliams@gmail.com>
2018-06-27 10:28:19 -0300

    API y avance fetch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-/* Holiii acá va tu código también */
+let bip = null;
+
+fetch("http://www.psep.cl/api/Bip.php?&numberBip=666")
+.then(response => response.json())
+.then(bipJASON => {
+  bip = bipJASON
+  console.log(bip);
+})
+.catch()
+
